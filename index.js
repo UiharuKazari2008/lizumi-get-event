89dc578d127455da3db4145bbca21f53c3a6914a
Yukimi Kazari <dev@9854.moe>
2020-12-06 15:33:12 -0500

    Finalising First version

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const icalExpander = require('ical-expander');
+const calExpander = require('ical-expander');
 const fs = require('fs');
 const https = require('https');
 const args = process.argv.slice(2)
 
 const calenderUrl = 'webcal://p31-caldav.icloud.com/published/2/MTAwMDA1MTk0NjEwMDAwNZb0-_EO5Cnp-ZlXblyvC1oGM5r9u_D_jMdLrYgLr4LmMqEW2MlgTraFMH60AD6KRXaFQeQSa_4MXcwWyRykHl4'
 
-
-function getCalendar(calUrl){
-    https.get(calUrl.replace('webcal://', 'https://'), (resp) => {
+function parseCalender() {
+    https.get(calenderUrl.replace('webcal://', 'https://'), (resp) => {
         resp.setEncoding('utf8');
         let data = '';
 
@@ -16,47 +15,47 @@ function getCalendar(calUrl){
         });
 
         resp.on('end', () => {
-            //console.log(data)
-            parseCalender(data);
+                const ical = new calExpander({
+                    ics: data,
+                    maxIterations: 1000
+                });
+                const now = new Date();
+                const start = new Date(now.getTime());
+                const end = new Date(now.getTime() + 60 * 60 * 1000);
+                const cal = ical.between(start, end);
+
+                if (cal && cal.events.length > 0) {
+                    let foundEvents = [];
+                    cal.events.forEach(function (event){
+                        if (event.component.jCal[1][6][3].includes("P1 ") || event.component.jCal[1][6][3].includes("P2 ") ) {
+                            foundEvents.push(event.component.jCal[1][6][3].replace("P1 ", "").replace("P2 ", "")) }
+                    })
+                    // Prints Current Airing Event
+                    if (foundEvents.length > 0) {
+                        console.log(foundEvents[0])
+                        process.exit(0);
+                    } else {
+                        console.log('NOEVENT')
+                        process.exit(0);
+                    }
+                } else {
+                    // No Events were found so get metadata from SXM
+                    console.log('NOEVENT')
+                    process.exit(0);
+                }
         });
 
     }).on('error', (err) => {
         if (err) {
-            console.log(`Failed to load calender: ${calenderUrl} with error ${err}`);
-            process.exit(1);
+            console.log('NOEVENT')
+            process.exit(0);
         }
     });
-}
-
-function parseCalender(data) {
-    const ical = new icalExpander({
-        ics: data,
-        maxIterations: 1000
-    });
-    const now = new Date();
-    const start = new Date(now.getTime());
-    const end = new Date(now.getTime() + 60 * 60 * 1000);
-    const cal = ical.between(start, end);
-
-    if (cal && cal.events.length > 0) {
-        let foundEvents = [];
-        cal.events.forEach(function (event){
-            if (event.component.jCal[1][6][3].includes("P1 ") || event.component.jCal[1][6][3].includes("P2 ") ) {
-                foundEvents.push(event.component.jCal[1][6][3].replace("P1 ", "").replace("P2 ", ""))
-            }
-        })
-
-        // Prints Current Airing Event
-        console.log(foundEvents[0])
-        process.exit(0);
-    } else {
-        // No Events were found so get metadata from SXM
-        console.log("No ongoing events")
-        process.exit(1);
-    }
 
 }
+process.on('uncaughtException', function(err) {
+    process.exit(1)
+});
 
 
-
-getCalendar(calenderUrl)
+parseCalender()
