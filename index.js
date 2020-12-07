bcaa01a366a26e54873b27524eea0bf5dfc1f52a
Yukimi Kazari <dev@9854.moe>
2020-12-06 23:36:14 -0500

    Update to support occurrences

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const calExpander = require('ical-expander');
-const fs = require('fs');
+//const fs = require('fs');
 const https = require('https');
-const args = process.argv.slice(2)
+//const args = process.argv.slice(2)
 
 const calenderUrl = 'webcal://p31-caldav.icloud.com/published/2/MTAwMDA1MTk0NjEwMDAwNZb0-_EO5Cnp-ZlXblyvC1qDRNXRRgiFqev0agb0RgqtE0kd9GenzfLNx8AAH5xbgfDXYxGT8WZ0KGr7a4Ld5S0'
 
@@ -15,34 +15,7 @@ function parseCalender() {
         });
 
         resp.on('end', () => {
-                const ical = new calExpander({
-                    ics: data,
-                    maxIterations: 1000
-                });
-                const now = new Date();
-                const start = new Date(now.getTime());
-                const end = new Date(now.getTime() + 60 * 60 * 1000);
-                const cal = ical.between(start, end);
-
-                if (cal && cal.events.length > 0) {
-                    let foundEvents = [];
-                    cal.events.forEach(function (event){
-                        if (event.component.jCal[1][6][3].includes("P1 ") || event.component.jCal[1][6][3].includes("P2 ") ) {
-                            foundEvents.push(event.component.jCal[1][6][3].replace("P1 ", "").replace("P2 ", "")) }
-                    })
-                    // Prints Current Airing Event
-                    if (foundEvents.length > 0) {
-                        console.log(foundEvents[0])
-                        process.exit(0);
-                    } else {
-                        console.log('NOEVENT')
-                        process.exit(0);
-                    }
-                } else {
-                    // No Events were found so get metadata from SXM
-                    console.log('NOEVENT')
-                    process.exit(0);
-                }
+            printResult(data)
         });
 
     }).on('error', (err) => {
@@ -53,7 +26,47 @@ function parseCalender() {
     });
 
 }
+
+function printResult(data){
+    const ical = new calExpander({
+        ics: data,
+        maxIterations: 1000
+    });
+    const now = new Date();
+    const start = new Date(now.getTime());
+    const end = new Date(now.getTime() + 30 * 60 * 1000);
+    const cal = ical.between(start, end);
+
+    //console.log(`start ${start} to ${end}`);
+    //console.log(cal.occurrences[0].item.component.jCal[1][3][3])
+
+    let foundEvents = [];
+    if (cal && cal.events.length > 0) {
+
+        cal.events.forEach(function (event){
+            if (event.component.jCal[1][6][3].includes("P1 ") || event.component.jCal[1][6][3].includes("P2 ") ) {
+                foundEvents.push(event.component.jCal[1][6][3].replace("P1 ", "").replace("P2 ", "")) }
+        })
+    } else if (cal && cal.occurrences.length > 0) {
+        cal.occurrences.forEach(function (event){
+            if (event.item.component.jCal[1][3][3].includes("P1 ") || event.item.component.jCal[1][3][3].includes("P2 ") ) {
+                foundEvents.push(event.item.component.jCal[1][3][3].replace("P1 ", "").replace("P2 ", "")) }
+        })
+    }
+
+    // Prints Current Airing Event
+    if (foundEvents.length > 0) {
+        console.log(foundEvents[0])
+        process.exit(0);
+    } else {
+        // No Events were found so get metadata from SXM
+        console.log('NOEVENT')
+        process.exit(0);
+    }
+}
+
 process.on('uncaughtException', function(err) {
+    console.log('NOEVENT')
     process.exit(1)
 });
 
