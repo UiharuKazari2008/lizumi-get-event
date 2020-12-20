9e689d23623661e68f8694e9cea1232a42585af4
Yukimi Kazari <dev@9854.moe>
2020-12-19 20:24:55 -0500

    Fixed the thing again

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const calExpander = require('ical-expander');
-//const fs = require('fs');
 const https = require('https');
-//const args = process.argv.slice(2)
 
 const calenderUrl = 'webcal://p31-caldav.icloud.com/published/2/MTAwMDA1MTk0NjEwMDAwNZb0-_EO5Cnp-ZlXblyvC1qDRNXRRgiFqev0agb0RgqtE0kd9GenzfLNx8AAH5xbgfDXYxGT8WZ0KGr7a4Ld5S0'
 
@@ -34,38 +32,37 @@ function printResult(data){
     });
     const now = new Date();
     const start = new Date(now.getTime());
-    const end = new Date(now.getTime() + 30 * 60 * 1000);
+    const end = new Date(now.getTime() + 1800 * 1000); // 30 Min Ahead
     const cal = ical.between(start, end);
 
     //console.log(`start ${start} to ${end}`);
-    //console.log(cal.occurrences[0].item.component.jCal[1][3][3])
 
-    let foundEvents = [];
-    if (cal && cal.events.length > 0) {
-
-        cal.events.forEach(function (event){
-            if (event.component.jCal[1][6][3].includes("P1 ") || event.component.jCal[1][6][3].includes("P2 ") ) {
-                foundEvents.push(event.component.jCal[1][6][3].replace("P1 ", "").replace("P2 ", "")) }
+    let foundEvents = []; // Blank list of events
+    Object.values(cal).forEach(function(cal_type){
+        cal_type.forEach(function(events){ // Process Single and Reoccurring Events
+            events.item.component.jCal[1].forEach(function (elements){ // Dig to the shit we actually want (fuck jcal)
+                if (elements[0] === 'summary') { // Bruteforce our way to the fucking summary element sense its not predictable
+                    if (elements[3].substring(0, 1) === 'P') { // Find Events that begin with "P", this is a internal naming if recordings events
+                        foundEvents.push(elements[3].substring(3)) // Remove the trigger term "P1 Profound Sounds" => "Profound Sounds"
+                    }
+                }
+            })
         })
-    } else if (cal && cal.occurrences.length > 0) {
-        cal.occurrences.forEach(function (event){
-            if (event.item.component.jCal[1][3][3].includes("P1 ") || event.item.component.jCal[1][3][3].includes("P2 ") ) {
-                foundEvents.push(event.item.component.jCal[1][3][3].replace("P1 ", "").replace("P2 ", "")) }
-        })
-    }
+    })
 
     // Prints Current Airing Event
     if (foundEvents.length > 0) {
         console.log(foundEvents[0])
         process.exit(0);
     } else {
-        // No Events were found so get metadata from SXM
+        // No Events were found so get metadata from SXM (Handled externally)
         console.log('NOEVENT')
         process.exit(0);
     }
 }
 
 process.on('uncaughtException', function(err) {
+    // Any error should always protect the output from anything that will mess up the bash scripts
     console.log('NOEVENT')
     process.exit(1)
 });
